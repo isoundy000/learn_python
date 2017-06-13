bff5780cb2276f7e433271b207880ca7f43e09ed
ghou <ghou@vmware.com>
2017-06-13 19:06:02 +0800

    scripte

---
+++ interview_questions/le_bo.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8-*-
+'''
+Created on 2017年6月13日
+
+@author: ghou
+'''
+# 两个骰子和为7的概率
+a = [(1, 6), (2, 5), (3, 4), (4, 3), (5, 2), (6, 1)]
+print len(a)/36
+
+# map, filter, reduce
+# ADBC 取最大字串
+
+# 获取jar的名字
+# .*\\(.*\.jar) 
